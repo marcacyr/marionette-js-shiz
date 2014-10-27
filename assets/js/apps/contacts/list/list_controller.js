a0c7baab51eb74a145bf2ac63d47116e06d809a0
Marc Cyr <marcacyr@gmail.com>
2014-10-27 16:24:22 -0700

    further along

--- assets/js/apps/contacts/list/list_controller.js
+++ assets/js/apps/contacts/list/list_controller.js
@@ -1,13 +1,46 @@
 ContactManager.module("ContactsApp.List", function(List, ContactManager, Backbone, Marionette, $, _){
   List.Controller = {
-    listContacts: function() {
-      var contacts = ContactManager.request("contact:entities");
+    listContacts: function(){
+      var loadingView = new ContactManager.Common.Views.Loading();
+      ContactManager.mainRegion.show(loadingView);
 
-      var contactsListView = new List.Contacts({
-        collection: contacts
+      var fetchingContacts = ContactManager.request("contact:entities");
+
+      $.when(fetchingContacts).done(function(contacts){
+        var contactsListView = new List.Contacts({
+          collection: contacts
+        });
+
+        contactsListView.on("childview:contact:show", function(childView, model){
+          ContactManager.trigger("contact:show", model.get("id"));
+        });
+
+        contactsListView.on("childview:contact:edit", function(childView, model){
+          var view = new ContactManager.ContactsApp.Edit.Contact({
+            model: model,
+            asModal: true
+          });
+
+          view.on("form:submit", function(data){
+            if(model.save(data)){
+              childView.render();
+              childView.flash("success");
+              ContactManager.dialogRegion.empty();
+            }
+            else{
+              view.triggerMethod("form:data:invalid", model.validationError);
+            }
+          });
+
+          ContactManager.dialogRegion.show(view);
+        });
+
+        contactsListView.on("childview:contact:delete", function(childView, model){
+          model.destroy();
+        });
+
+        ContactManager.mainRegion.show(contactsListView);
       });
-
-      ContactManager.mainRegion.show(contactsListView);
     }
   };
 });
--- assets/js/apps/contacts/list/list_view.js
+++ assets/js/apps/contacts/list/list_view.js
@@ -4,11 +4,47 @@ ContactManager.module("ContactsApp.List", function(List, ContactManager, Backbon
     template: "#contact-list-item",
 
     events: {
-      "click": "highlightName"
+      "click": "highlightName",
+      "click td a.js-show": "showClicked",
+      "click td a.js-edit": "editClicked",
+      "click button.js-delete": "deleteClicked"
+    },
+
+    flash: function(cssClass){
+      var $view = this.$el;
+      $view.hide().toggleClass(cssClass).fadeIn(800, function(){
+        setTimeout(function(){
+          $view.toggleClass(cssClass);
+        }, 500);
+      });
     },
 
     highlightName: function() {
       this.$el.toggleClass("warning");
+    },
+
+    showClicked: function(e) {
+      e.preventDefault();
+      e.stopPropagation();
+      this.trigger("contact:show", this.model);
+    },
+
+    editClicked: function(e) {
+      e.preventDefault();
+      e.stopPropagation();
+      this.trigger("contact:edit", this.model);
+    },
+
+    deleteClicked: function(e) {
+      e.stopPropagation();
+      this.trigger("contact:delete", this.model);
+    },
+
+    remove: function(){
+      var self = this;
+      this.$el.fadeOut(function() {
+        Marionette.ItemView.prototype.remove.call(self);
+      });
     }
   });
 
